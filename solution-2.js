8918e99b26d473efd7408ddae8f436fe50b5c46a
Bill <bill.kurtson@sj.se>
2019-12-02 21:55:26 +0100

    day 2 part 2 solution

--- solution-2.js
+++ solution-2.js
@@ -8,40 +8,94 @@ const testInput = require('fs')
             .toString()
             .split(',')
             .map(Number);
-let processedCode = [...fInput];
-let answer = 0;
+let processedCode;
+let answerPart1 = 0;
+let answerPart2;
 const OPS = {
     1: add,
     2: multiply,
     99: terminate
 };
-
 // preprocess input - forgot this part at first which made me very confused about not being right
-processedCode[1] = 12;
-processedCode[2] = 2;
+fInput[1] = 12;
+fInput[2] = 2;
 
-for (let i = 0; i < fInput.length; i += 4) {
-    const instruction = processedCode[i];
-    const op = OPS[instruction];
-    const val1 = processedCode[processedCode[i + 1]];
-    const val2 = processedCode[processedCode[i + 2]];
-    const targetIndex = processedCode[i + 3];
-    const res = op(val1, val2);
+processedCode = [...fInput];
 
-    if (res === false) {
-        break;
+function runIntcode(memorySegment) {
+    for (let i = 0; i < fInput.length; i += 4) {
+        const instruction = processedCode[i];
+        const op = OPS[instruction];
+        const val1 = processedCode[processedCode[i + 1]];
+        const val2 = processedCode[processedCode[i + 2]];
+        const targetIndex = processedCode[i + 3];
+        const res = op(val1, val2);
+
+        if (res === false) {
+            break;
+        }
+
+        processedCode[targetIndex] = res;
     }
 
-    processedCode[targetIndex] = res;
+    return processedCode;
 }
 
-answer = processedCode[0];
+function part1() {
+    const finishedMemory = runIntcode(fInput);
 
-console.group('answer')
-console.log(answer);
-console.groupEnd('answer')
+    answerPart1 = finishedMemory[0];
 
-return answer;
+    console.assert(answerPart1 === 3306701, `${answerPart1} should be 3306701`);
+
+    console.group('answer 1');
+    console.log(answerPart1);
+    console.groupEnd('answer 1');
+
+    return answerPart1;
+}
+
+function part2() {
+    let noun = 0;
+    let verb = 0;
+    let target = 19690720;
+    let result;
+
+    processedCode = [...fInput];
+    processedCode[1] = noun;
+    processedCode[2] = verb;
+
+    for (let i = 0; i <= 99; ++i) {
+        for (let j = 0; j <= 99; ++j) {
+            processedCode = [...fInput];
+            processedCode[1] = i;
+            processedCode[2] = j;
+
+            result = runIntcode(fInput)
+
+            if (result[0] === target) {
+                answerPart2 = (100 * i) + j;
+
+                break;
+            }
+        }
+
+        if (answerPart2) {
+            break;
+        }
+    }
+
+    console.assert(answerPart2 === 7621, `${answerPart2} should be 7621`);
+
+    console.group('answer 2');
+    console.log(answerPart2);
+    console.groupEnd('answer 2');
+
+    return answerPart2;
+}
+
+part1();
+part2();
 
 function add(num1, num2) {
     return num1 + num2;
